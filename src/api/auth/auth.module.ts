1e875f38c9cdacc474594d7adc364a49a42298e4
Соколовский Иван Павлович <ivan3177@gmail.com>
2023-07-05 19:10:20 +0400

    refactor: Minor improvements
    Remove global app service and controller, add missing Passport module import

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -5,11 +5,13 @@ import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtConfigService } from './jwt.service';
 import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     UserModule,
     JwtModule.registerAsync({ useClass: JwtConfigService }),
+    PassportModule,
   ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
