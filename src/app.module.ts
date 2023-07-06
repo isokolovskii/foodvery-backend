679a41f423cffb792fd5054d1b5f047e877bfae7
Соколовский Иван Павлович <ivan3177@gmail.com>
2023-07-06 16:52:26 +0400

    patch(app): Add mail module and event emmiter to application

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { getEnvPath } from './common/helper/env.helper';
 import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
 import { ApiModule } from './api/api.module';
+import { MailModule } from './mail/mail.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
@@ -11,7 +13,9 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
   imports: [
     ConfigModule.forRoot({ envFilePath, isGlobal: true }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    EventEmitterModule.forRoot(),
     ApiModule,
+    MailModule,
   ],
   controllers: [],
   providers: [],
